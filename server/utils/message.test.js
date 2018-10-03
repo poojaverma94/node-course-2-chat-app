6d1ed692ac5f62d862dea39c22e97fb23f18507b
pooja verma <poojaverma2094@gmail.com>
2018-10-03 00:12:25 -1000

    create generateMessage utility

---
+++ server/utils/message.test.js
@@ -0,0 +1,28 @@
+var expect = require('expect');
+
+var {generateMessage} = require('./message');
+
+describe('generateMessage', () => {
+  it ('should generate correct message object', () => {
+    var from = 'jen';
+    var text = 'some message';
+    var message = generateMessage(from,text);
+    expect(message.createdAt).toBeA('number');
+    expect(message).toInclude({from,text});
+  });
+});
+
+/*var expect = require('expect');
+
+var {generateMessage} = require('./message');
+
+describe('generateMessage', () => {
+  it('should generate correct message object', () => {
+    var from = 'Jen';
+    var text = 'Some message';
+    var message = generateMessage(from, text);
+
+    expect(message.createdAt).toBeA('number');
+    expect(message).toInclude({from, text});
+  });
+});*/
