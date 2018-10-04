e9707c9916d5fa2f756bbac8660b3a3f6d6c5146
pooja verma <poojaverma2094@gmail.com>
2018-10-04 09:02:55 -1000

    Add join page that submit to chat.html

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,5 +1,23 @@
 var socket = io();
 
+function scrollButton(){
+  //selectors
+  var messager=jQuery('#messages');
+  var newMessage = message.children('li:last_child');
+  //Heights
+
+  var clientHeight = message.prop('clientHeight');
+  var scrollTop= message.prop('scrollTop');
+  var scrollHeight = message.prop('scrollHeight');
+
+  var newMessageHeight = newMessage.innerHeight();
+  var lastMessageHeight = newMessage.prev().innerHeight();
+
+  if(clientHeight+scrollTop+newMessageHeight+lastMessageHeight>=scrollHeight)
+  {
+    message.scrollTop(scrollHeight);
+  }
+}
 socket.on('connect', function () {
   console.log('Connected to server');
 });
